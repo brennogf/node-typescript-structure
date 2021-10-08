ee801a5e543234b6c101e5776b4bb1712e241eaf
Brenno Givigier <brennogivigier@gmail.com>
2021-10-07 21:09:45 -0300

    Updated

--- src/tests/FirstTest.spec.ts
+++ src/tests/FirstTest.spec.ts
@@ -1,9 +1,3 @@
-import { User } from '@models/User'
-
-test('it should be ok', () => {
-  const user = new User()
-
-  user.name = 'Diego'
-
-  expect(user.name).toEqual('Diego')
+test('test', () => {
+  expect('teste').toEqual('teste')
 })
